7bad8311bd360a5f59a6947e49f384e0cdd4f784
Juri <Chau-ying-kot@hotmail.com>
2019-06-08 20:56:41 +0200

    Implementation de addEdge et adjacents Changement de Configuration, on peut pas passer un tableau statique dans les liste Modification pour une map
    Peut être revoir les struct, BSF m'a l'air compliqué à implémenter actuellement

--- main.cpp
+++ main.cpp
@@ -1,31 +1,54 @@
-
-
 #include <cstdlib>
 #include <cstdio>
+#include <list>
+#include <queue>
+#include <map>
+#include <vector>
 
 using namespace std;
 
 const unsigned TAILLE_GRILLE = 3;
-using sommet = unsigned[TAILLE_GRILLE][TAILLE_GRILLE];
+using Index = int;
+using Configuration = array<array<unsigned , TAILLE_GRILLE>,TAILLE_GRILLE>;
+//= unsigned[TAILLE_GRILLE][TAILLE_GRILLE];
+using Sommets = map<Index, Configuration>;
 
 
-class GraphConfigs{
+class GraphConfigs {
 private:
-   queue<sommet> sommetsExistants;
-   using parents = queue<sommet> ;
-   
-   createChlidren(sommet);
-   const list<sommet> adjacents(sommet);
-   void addEdge(sommet s);
-   parents BFS(sommet s);
-   bool checkSolution(sommet s);
-   void configInit(sommet);
-   void affichageSolution(parents p);
+    vector<list<Index>> adjcendyLists;
+
+public:
+
+    void createChlidren(Configuration, Sommets);
+
+    const list<Index> adjacents(Index index) const;
+
+    void addEdge(Index index1, Index index2);
+
+    Index BFS(Sommets sommets);
+
+    bool checkSolution(Configuration s);
+
+    void configInit(Configuration);
+
+    void affichageSolution(Sommets) const;
 };
+
+void GraphConfigs::addEdge(Index index1, Index index2) {
+    adjcendyLists.at(index1).push_back(index2);
+    if (index1 != index2) {
+        adjcendyLists.at(index2).push_back(index1);
+    }
+}
+
+const list<Index> GraphConfigs::adjacents(Index index) const {
+    return adjcendyLists.at(index);
+}
 
 int main() {
 
-   
-   
-   return 0;
+
+    return 0;
 }
+
