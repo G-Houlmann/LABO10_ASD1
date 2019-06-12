5ebcc8bb1a71fed352a987d9d161e7b3f52b2899
KurohanaJuri <Chau-ying-kot@hotmail.com>
2019-06-12 13:32:21 +0200

    CheckSol et Init

--- main.cpp
+++ main.cpp
@@ -4,33 +4,36 @@
 #include <queue>
 #include <map>
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
 const unsigned TAILLE_GRILLE = 3;
 using Index = int;
-using Configuration = array<array<unsigned , TAILLE_GRILLE>,TAILLE_GRILLE>;
+using Configuration = array<array<unsigned, TAILLE_GRILLE>, TAILLE_GRILLE>;
 //= unsigned[TAILLE_GRILLE][TAILLE_GRILLE];
-using Sommets = map<Index, Configuration>;
+using Sommets = map<Configuration, Configuration>; // ConfigCourant,configParent
 
 
 class GraphConfigs {
 private:
     vector<list<Index>> adjcendyLists;
+    Sommets sommetList;
+    Configuration configurationInitial;
 
 public:
 
-    void createChlidren(Configuration, Sommets);
+    void createChlidren(Configuration);
 
     const list<Index> adjacents(Index index) const;
 
     void addEdge(Index index1, Index index2);
 
-    Index BFS(Sommets sommets);
+    Index BFSAvecParent() const;
 
-    bool checkSolution(Configuration s);
+    bool checkSolution(Configuration s) const;
 
-    void configInit(Configuration);
+    void configInit();
 
     void affichageSolution(Sommets) const;
 };
@@ -46,8 +49,30 @@ const list<Index> GraphConfigs::adjacents(Index index) const {
     return adjcendyLists.at(index);
 }
 
+bool GraphConfigs::checkSolution(Configuration s) const {
+    for (size_t i = 0; i < TAILLE_GRILLE; ++i) {
+        for (size_t j = 0; j < TAILLE_GRILLE; ++j) {
+            if (s.at(i).at(j) != i * TAILLE_GRILLE + j) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+void GraphConfigs::configInit() {
+    for (size_t i = 0; i < TAILLE_GRILLE; ++i) {
+        for (size_t j = 0; j < TAILLE_GRILLE; ++j) {
+            cin >> configurationInitial.at(i).at(j);
+        }
+    }
+}
+
 int main() {
 
+    GraphConfigs configs;
+
+    configs.configInit();
 
     return 0;
 }
